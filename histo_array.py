46fe481021e74340ec87717c02af04609ea9725f
Lea Jouvin <lea.jouvin@gmail.com>
2016-04-29 11:28:35 +0200

    works avec $HESSCONFIG

--- histo_array.py
+++ histo_array.py
@@ -5,20 +5,21 @@ import root_numpy as nr
 from root_numpy import hist2array
 from root_numpy.testdata import get_filepath
 import argparse
+import os
 
 """
 For one specific config, store the IRF on 4D numpy table for each value of the Zenithal angle, Offset, Efficiency and Energy used for the MCs simulation
-Example of commande line to run to create this 4D table with the directory of the MC simulation output and the config name as argument
-./histo_array.py '/Users/jouvin/Desktop/these/WorkGAMMAPI/IRF/Brunoconfig' 'elm_south_stereo_Prod15_5'
-./histo_array.py '/Users/jouvin/Desktop/these/WorkGAMMAPI/IRF/Brunoconfig' 'elm_north_stereo_Prod15_5'
+Example of commande line to run to create this 4D table where you give the config name as argument
+./histo_array.py 'elm_south_stereo_Prod15_5'
+./histo_array.py 'elm_north_stereo_Prod15_5'
 """
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Store the IRF from Mc simulation in a 4D numpy table')
-    parser.add_argument('directory', action="store", help='directory of the IRFs obtained from the MCs simulation')
+    #parser.add_argument('directory', action="store", help='directory of the IRFs obtained from the MCs simulation')
     parser.add_argument('config', action="store", help='Config')
     results = parser.parse_args()
-    print "Store the IRF in a 4D table from the MC simulations in ", results.directory , " and for the config ", results.config
+    print "Store the IRF in a 4D table from the MC simulations in ",  os.path.expandvars('$HESSCONFIG') , " and for the config ", results.config
 
     # Zenithal angle, offset, efficiency and energy used to simulate the MCs in HAP-fr
     enMC = [0.02, 0.03, 0.05, 0.08, 0.125, 0.2, 0.3, 0.5, 0.8, 1.25, 2, 3, 5, 8, 12.5, 20, 30, 50, 80, 125]
@@ -39,8 +40,8 @@ if __name__ == '__main__':
 
     # In Hap-FR: 20 true energies used for the MC simulation
     # In the .root histogram for the effective area and the resolution there are more than 20 energy bins so we take one MC simulation and we determine to which index in these histograms match the MC energy.
-    filehistoarea = results.directory + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff100/CollectionArea.root"
-    filehistoresol = results.directory + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff100/EnergyResolution.root"
+    filehistoarea = os.path.expandvars('$HESSCONFIG') + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff100/CollectionArea.root"
+    filehistoresol = os.path.expandvars('$HESSCONFIG') + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff100/EnergyResolution.root"
     name_hist_area = "EffArea_67deg_2.5off_eff100_FixedE"
     name_hist_biais = "Resol_Biais_67deg_2.5off_eff100_FixedE"
     TFileArea = ROOT.TFile(filehistoarea)
@@ -59,9 +60,9 @@ if __name__ == '__main__':
             print off
             for (izen, zen) in enumerate(zenMC):
                 print zen
-                filehistoarea = results.directory + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff" + str(
+                filehistoarea =  os.path.expandvars('$HESSCONFIG') + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff" + str(
                     eff) + "/CollectionArea.root"
-                filehistoresol = results.directory + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff" + str(
+                filehistoresol =  os.path.expandvars('$HESSCONFIG') + "/" + results.config + "/gFixedEnergy_paris_0-8-8-8_CamOptimal_hwtrig_eff" + str(
                     eff) + "/EnergyResolution.root"
                 TFileArea = ROOT.TFile(filehistoarea)
                 TFileResol = ROOT.TFile(filehistoresol)
@@ -82,6 +83,6 @@ if __name__ == '__main__':
                 TableArea[:, ioff, izen, ieff] = AreaArray
                 TableBiais[:, ioff, izen, ieff] = AreaBiais
                 TableSigma[:, ioff, izen, ieff] = AreaSigma
-    outdir="output_4Dnumpyarrays"
+    outdir=os.path.expandvars('$HESSCONFIG')+"/"+results.config
     np.savez(outdir+"/IRF_"+results.config+".npz", TableArea=TableArea, TableBiais=TableBiais, TableSigma=TableSigma, enMC=enMC, lnenMC=lnenMC,
              zenMC=zenMC, offMC=offMC, effMC=effMC)
